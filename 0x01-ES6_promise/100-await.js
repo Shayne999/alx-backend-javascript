f45205f10b2a235d2ca4c45176a5ad246f303724
Shayne999 <shaynejuniorn@gmail.com>
2024-06-25 21:17:33 +0200

    more fixes

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,20 +1,14 @@
-import {uploadPhoto, createUser} from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
-async function asyncUploadUser() {
+export default async function asyncUploadUser() {
+  let res = {};
 
-    try {
-        const photoRes = await uploadPhoto();
-        const userRes = await createUser();
-        return {
-            photo: photoRes,
-            user: userRes
-        };
-
-    } catch (error) {
-        return {
-            photo: null,
-            user: null
-          };
-    }
-
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    res = { photo, user };
+  } catch (err) {
+    res = { photo: null, user: null };
+  }
+  return res;
 }
--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,13 +1,13 @@
-function guardrail(mathFunction){
+export default function guardrail(mathFunction) {
     const queue = [];
-
+  
     try {
-        queue.push(mathFunction());
-    } catch (error) {
-        queue.push(error);
+      queue.push(mathFunction());
+    } catch (err) {
+      queue.push(String(err));
     } finally {
-        queue.push('Guardrail was processed');
+      queue.push('Guardrail was processed');
     }
-
+  
     return queue;
-}
+  }
