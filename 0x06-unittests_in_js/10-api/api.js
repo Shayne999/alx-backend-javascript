e479b41da18da2068116ba8c7af9e9c9bf7539df
Shayne999 <shaynejuniorn@gmail.com>
2024-08-22 13:12:08 +0200

    10 Typo and test Fixes

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -15,17 +15,19 @@ app.get('/cart/:id(\\d+)', (req, res) => {
 app.get('/available_payments', (_req, res) => {
     res.json({ payment_methods: { credit_cards: true, paypal: false } });
 });
+  
+app.post('/login', (req, res) => {
+let username = '';
 
-app.post('/login', (req, res,) => {
-    let username = '';
-    if (req.body){
-        username = req.body.userName;
-    }
-    res.send(`Welcome ${username}`);
+if (req.body) {
+    username = req.body.userName;
+}
+
+res.send(`Welcome ${username}`);
 });
 
 app.listen(PORT, () => {
-    console.log(`API available on localhost port ${PORT}`);
+console.log(`API available on localhost port ${PORT}`);
 });
 
 module.exports = app;
--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -34,20 +34,19 @@ describe('API integration test', () => {
         });
     });
 
-    it('POST /login returns valid response', (done) => {
-        request.post(`${API_URL}/login`, {json: {userName: 'Pinkbrook'}}, (_err, res, body) => {
-          expect(res.statusCode).to.be.equal(200);
-          expect(body).to.be.equal('Welcome Pinkbrook');
-          done();
+    it('POST /login should return a valid response', (done) => {
+        request.post(`${API_URL}/login`, {json: {userName: 'TestName'}}, (_err, res, body) => {
+            expect(res.statusCode).to.be.equal(200);
+            expect(body).to.be.equal('Welcome TestName');
+            done();
         });
-      });
-    
-      it('GET /available_payments returns valid response', (done) => {
+    });
+
+    it('GET /available_payments should return a valid response', (done) => {
         request.get(`${API_URL}/available_payments`, (_err, res, body) => {
-          expect(res.statusCode).to.be.equal(200);
-          expect(JSON.parse(body))
-            .to.be.deep.equal({payment_methods: {credit_cards: true, paypal: false}});
-          done();
+            expect(res.statusCode).to.be.equal(200);
+            expect(JSON.parse(body)).to.be.deep.equal({payment_methods: {credit_cards: true, paypal: false}});
+            done();
         });
     });
 });
